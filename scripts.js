a30c323a116e0d906783ede6a901284b391a22af
Matt Doyle <matthew.doyle@amaroo.co>
2018-04-18 06:14:31 +1000

    Refactor to use Section class

--- scripts.js
+++ scripts.js
@@ -2,76 +2,83 @@
 const projectTitle = "Cochlear Remote Assistance (iOS)";
 const projectAuthor = "Matt Doyle";
 
+// Create section class
+class Section {
+  constructor(sectionTitle) {
+    this.sectionName = sectionTitle.replace(/\s/g, '-');
+    this.sectionName = this.sectionName.toLowerCase();
+    this.sectionTitle = sectionTitle;
+    this.target = document.querySelector("#toc");
+    this.div = document.createElement("div");
+    this.div.id = this.sectionName;
+    this.div.innerHTML = `<h1>${sectionTitle}</h1>`;
+    this.div.className = "section";
+    this.target.parentNode.insertBefore(this.div, this.target.lastSibling);
+  }
+
+  createTitlePage() {
+    // TODO
+  }
+
+  createToc() {
+    // TODO
+  }
+
+  addParagraph (paragraph) {
+    const target = document.querySelector(`#${this.sectionName}`);
+    const newParagraph = document.createElement("p");
+    newParagraph.className = "section__paragraph";
+    newParagraph.innerHTML = paragraph;
+    target.appendChild(newParagraph);
+  }
+
+  addImage (source, style = "default", size = "medium") {
+    const target = document.querySelector(`#${this.sectionName}`);
+    const newImage = document.createElement("img");
+    newImage.src = source;
+    target.appendChild(newImage);
+  }
+
+  addImageGrid ([source], percentMaxHeight) {
+    // TODO
+  }
+
+  addCodeBlock (codeBlock) {
+    // TODO
+  }
+
+  addOrderedList ([listItem]) {
+    // TODO
+  }
+
+  addUnorderedList ([listItem]) {
+    // TODO
+  }
+
+  addTool ([tool]) {
+    // TODO
+  }
+
+  addColorPallet ([color]) {
+    // TODO
+  }
+
+  addTypeFace(typeface) {
+    // TODO
+  }
+
+  addUserStory([userStory]) {
+    // TODO
+  }
+
+}
+
 // Define functions
-const createTitlePage = () => {
-  // TODO
-}
-
-const createToc = () => {
-  // TODO
-}
-
-const updateProjectName = () => {
+updateProjectName = () => {
   elements = document.getElementsByClassName('project-name');
   for (element of elements) {
     element.innerHTML = projectTitle;
   }
-}
-
-const addSection = (sectionName, sectionTitle) => {
-  const target = document.querySelector("#toc");
-  const div = document.createElement("div");
-  div.id = sectionName;
-  div.innerHTML = `<h1>${sectionTitle}</h1>`;
-  div.className = "section"
-  target.parentNode.insertBefore(div, target.lastSibling);
-}
-
-const addParagraph = (sectionName, paragraph) => {
-  const target = document.querySelector(`#${sectionName}`);
-  const newParagraph = document.createElement("p");
-  newParagraph.className = "section__paragraph";
-  newParagraph.innerHTML = paragraph;
-  target.appendChild(newParagraph);
-}
-
-const addImage = (sectionName, source, style = "default", size = "medium") => {
-  const target = document.querySelector(`#${sectionName}`);
-  const newImage = document.createElement("img");
-  newImage.src = source;
-  target.appendChild(newImage);
-}
-
-const addImageGrid = (sectionName, [source], percentMaxHeight) => {
-  // TODO
-}
-
-const addCodeBlock = (sectionName, codeBlock) => {
-  // TODO
-}
-
-const addOrderedList = (sectionName, [listItem]) => {
-  // TODO
-}
-
-const addUnorderedList = (sectionName, [listItem]) => {
-  // TODO
-}
-
-const addTool = (sectionName, [tool]) => {
-  // TODO
-}
-
-const addColorPallet = (sectionName, [color]) => {
-  // TODO
-}
-
-const addTypeFace = (sectionName, typeface) => {
-  // TODO
-}
-
-const addUserStory = (sectionName, [userStory]) => {
-  // TODO
 }
 
 // Update DOM
