61f64933e0a9492e0d354ea3971136264cfcf8a2
Matt Doyle <matthew.doyle@amaroo.co>
2018-04-17 11:30:57 +1000

    Add title and author parameters to .js file

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 // Configure document
-let projectTitle = "PDF portfolio exporter";
-let projectAuthor = "Matt Doyle";
+const projectTitle = "PDF portfolio exporter";
+const projectAuthor = "Matt Doyle";
 
+// Define functions
+const updateTocHeader = () => {
+  elements = document.getElementsByClassName('project-name');
+  for (element of elements) {
+    element.innerHTML = projectTitle;
+  }
+}
+
+// Update DOM
 document.title = projectTitle;
+document.author = projectAuthor;
+
+updateTocHeader();
