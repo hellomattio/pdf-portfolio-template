ae9e126168003d89157d4319eb211efaf136b751
Matt Doyle <matthew.doyle@amaroo.co>
2018-04-30 10:49:30 +1000

    Remove unneeded comments from .js file

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-// // Configure document
-// const projectTitle = "Software Project PDF Generator";
-// const projectSubtitle = "A JavaScript tool to generate beautiful PDF documents for your projects.";
-// const projectAuthor = "Matt Doyle";
-// const authorPhone = "0407 417 405"
-// const authorEmail = "hello@matt.fyi"
-// const coverImageSource = "assets/main.png";
-
 // Create section class
 class Section {
   constructor(sectionTitle = "Error: section name not defined") {
