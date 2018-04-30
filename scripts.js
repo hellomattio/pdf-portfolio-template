38af50438ae3628921a9cca842a18669478a5119
Matt Doyle <matthew.doyle@amaroo.co>
2018-04-30 10:42:29 +1000

    Separate config variables into new file

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-// Configure document
-const projectTitle = "Software Project PDF Generator";
-const projectSubtitle = "A JavaScript tool to generate beautiful PDF documents for your projects.";
-const projectAuthor = "Matt Doyle";
-const authorPhone = "0407 417 405"
-const authorEmail = "hello@matt.fyi"
-const coverImageSource = "assets/main.png";
+// // Configure document
+// const projectTitle = "Software Project PDF Generator";
+// const projectSubtitle = "A JavaScript tool to generate beautiful PDF documents for your projects.";
+// const projectAuthor = "Matt Doyle";
+// const authorPhone = "0407 417 405"
+// const authorEmail = "hello@matt.fyi"
+// const coverImageSource = "assets/main.png";
 
 // Create section class
 class Section {
